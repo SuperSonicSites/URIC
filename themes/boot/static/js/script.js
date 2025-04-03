76e3e070546435536988bd1ec59b41245effcd60
Supersonic Sites <64860831+SuperSonicSites@users.noreply.github.com>
2025-04-03 15:54:27 -0400

    Update script.js

--- themes/boot/static/js/script.js
+++ themes/boot/static/js/script.js
@@ -33,17 +33,3 @@ $(document).on("click", '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox();
   });
-
-document.addEventListener('DOMContentLoaded', function() {
-  // Select all <a> elements inside the .desktopmenu container
-  const links = document.querySelectorAll('.desktopmenu a');
-  
-  // Check if any links are found
-  if (links.length > 0) {
-    // Select the last link
-    const lastLink = links[links.length - 1];
-    
-    // Append the classes to the last link
-    lastLink.classList.add('btn', 'btn-success', 'btn-lg');
-  }
-});
